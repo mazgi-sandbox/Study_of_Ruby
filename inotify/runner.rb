8cd1ad17fb477a499603dcaf5d74cf1ef0819723
mazgi <info@mazgi.info>
2012-06-19 15:05:17 +0900

    rb-inotify使ってみた

--- inotify/runner.rb
+++ inotify/runner.rb
@@ -1,7 +1,24 @@
 # -*- encoding: utf-8 -*-
 
+require "rb-inotify"
+#require "ruby-inotify"
+
 require 'pp'
 
-Dir::glob("event_source/**/*").each {|f|
+EVENT_SOURCE='event_source/**/*'
+
+Dir::glob(EVENT_SOURCE).each {|f|
   pp f
 }
+
+
+notifier = INotify::Notifier.new
+#notifier.watch("path/to/foo.txt", :modify) {puts "foo.txt was modified!"}
+notifier.watch("event_source", :all_events) do |event|
+  #pp event
+  puts event.name
+  puts event.watcher.path
+  puts "#{event.name} is now in #{EVENT_SOURCE}."
+end
+notifier.run
+
