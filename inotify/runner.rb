abd1755538f320094e886d5b76dcbd951896f0dc
mazgi <info@mazgi.info>
2012-06-20 16:56:45 +0900

    それっぽくディレクトリを監視するようにしてみた

--- inotify/runner.rb
+++ inotify/runner.rb
@@ -7,18 +7,63 @@ require 'pp'
 
 EVENT_SOURCE='event_source/**/*'
 
-Dir::glob(EVENT_SOURCE).each {|f|
-  pp f
-}
+class Watcher
+  def initialize(path='')
+    @path=path
+    @files=[]
+    @running=false
+  end
 
+  def start
+    puts "watch start with #{@path}"
+    counter=0
+    @running=true
+    Dir::glob(@path).each {|f|
+      @files.push f
+    }
+    @thread = Thread.new do
+      while @running
+        if counter%60==0
+          puts '<<<begin thread block>>>'
+          puts "watching ... #{@path}"
+          puts "#{@files.join(",\n")}"
+          puts '<<<end thread block>>>'
+	end
+	counter+=1
+        sleep 1
+      end
+    end
+    @thread.run
+  end
+
+  def stop
+    @running=false
+    @thread.join
+    @files.clear
+    puts "watch stop with #{@path}"
+  end
+end
+
+w = Watcher.new(EVENT_SOURCE)
+w.start
 
 notifier = INotify::Notifier.new
+#pp notifier.methods
 #notifier.watch("path/to/foo.txt", :modify) {puts "foo.txt was modified!"}
-notifier.watch("event_source", :all_events) do |event|
+notifier.watch("event_source", :recursive, :create, :delete, :attrib) do |event|
+  puts '<<<begin inotify event block>>>'
+  p Time.now
+
   #pp event
-  puts event.name
-  puts event.watcher.path
-  puts "#{event.name} is now in #{EVENT_SOURCE}."
+  puts "event.name:#{event.name}"
+  puts "event.watcher.path:#{event.watcher.path}"
+  #puts "#{event.name} is now in #{EVENT_SOURCE}."
+
+  w.stop
+  w.start
+
+  puts '<<<end inotify event block>>>'
 end
+
 notifier.run
 
