647574609a570ee97b4f0df4547cefb377992acf
mazgi <info@mazgi.info>
2012-06-21 10:36:53 +0900

    基底ディレクトリ取れるようになったっぽい

--- inotify/glob.rb
+++ inotify/glob.rb
@@ -3,6 +3,14 @@
 require 'pp'
 
 EVENT_SOURCE='event_source/**/*'
+#EVENT_SOURCE='/var/**/tmp'
+
+base_dir=EVENT_SOURCE
+while true
+  break unless base_dir.index('*')
+  base_dir=File::dirname(base_dir)
+end
+puts "basedir:#{base_dir}"
 
 paths=Dir::glob(EVENT_SOURCE).map{|path|
   case File::ftype(path)
@@ -16,6 +24,6 @@ paths=Dir::glob(EVENT_SOURCE).map{|path|
 paths.uniq!
 paths.sort!{|a,b|a.size<=>b.size}
 paths.each{|path|
-  puts path
+  puts "subdirs:#{path}"
 }
 
