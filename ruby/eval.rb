ff16d48603caccd932320d2d379e964c682269d2
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2012-07-14 20:52:35 +0900

    特定のインスタンスだけ振る舞い変えてみた

--- ruby/eval.rb
+++ ruby/eval.rb
@@ -13,12 +13,23 @@ p a.format 'hoe'
 
 Formatter.class_eval do
   def format_wrapper(str)
-    '<wrap>' + format_orig(str) + '</wrap>'
+    if respond_to?(:obj_format_wrapper)
+      obj_format_wrapper str
+    else
+      '<wrap>' + format_orig(str) + '</wrap>'
+    end
   end
   alias_method :format_orig, :format
   alias_method :format, :format_wrapper
 end
 
-p a.format 'hoe'
+p a.format 'aaa'
 b = Formatter.new
-p b.format 'hoe'
+p b.format 'bbb'
+c = Formatter.new
+c.instance_eval do
+  def obj_format_wrapper(str)
+    '<obj_wrap>' + format_orig(str) + '</obj_wrap>'
+  end
+end
+p c.format 'ccc'
